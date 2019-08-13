da1f05d13e5847470ed8910f9da7462702511e30
David Chang <david@iotechsys.com>
2019-08-13 06:42:12 +0000

    Add version 3

--- hello.go
+++ hello.go
@@ -1,16 +1,13 @@
 package hello
 
-import (
-	"rsc.io/quote"
-	quoteV3 "rsc.io/quote/v3"
-)
+import "rsc.io/quote/v3"
 
 // Export function Hello()
 func Hello() string {
-	return quote.Hello()
+	return quote.HelloV3()
 }
 
 // Exprot function Proverb()
 func Proverb() string {
-	return quoteV3.Concurrency()
+	return quote.Concurrency()
 }
